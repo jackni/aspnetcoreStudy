889532be76f7a41e4610d2269d48e457117b6ffb
jack.ni <jack.ni@gmail.com>
2017-12-13 23:42:10 +1030

    demo oidc checkin

--- NetCoreWebApi/IdentityWeb/Clients.cs
+++ NetCoreWebApi/IdentityWeb/Clients.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,24 @@ namespace IdentityWeb
                         new Secret("password".Sha256()) //client private key
                     },
                     AllowedScopes = new List<string> {"customAPI.read"}
+                }
+                ,
+                new Client {
+                    ClientId = "openIdConnectClient",
+                    ClientName = "Example Implicit Client Application",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "customAPI.write"
+                    },
+                    //https://localhost:44357/
+                    //https://localhost:44302
+                    RedirectUris = new List<string> {"https://localhost:44357/signin-oidc"},
+                    PostLogoutRedirectUris = new List<string> {"https://localhost:44357"}
                 }
             };
         }
--- NetCoreWebApi/IdentityWeb/Startup.cs
+++ NetCoreWebApi/IdentityWeb/Startup.cs
@@ -31,19 +31,19 @@ namespace IdentityWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseIdentityServer();
-            app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello Identity Server!");
-            });
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello Identity Server!");
+            //});
         }
     }
 }
